2e40d470316582adf2c962bc124770074c9cf3b2
AnshikaOel <anshika.oel@gmail.com>
2023-10-04 17:11:42 +0530

    main

---
+++ script.js
@@ -0,0 +1,47 @@
+// const res = require("express/lib/response")
+const apiKey="6c1fe840780ba792cf24fa2b18547c50"
+const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchbox=document.querySelector(".search input")
+const searchbtn=document.querySelector(".search button")
+const weatherIcon=document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response=await fetch(apiURL+city+`&appid=${apiKey}`)
+    if(response.status==404)
+    {
+        document.querySelector(".error").style.display="block"
+        document.querySelector(".weather").style.display="none"
+    }else{
+    var data=await response.json()
+    console.log(data)
+    document.querySelector(".city").innerHTML=data.name
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C"
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%"
+    document.querySelector(".wind").innerHTML=data.wind.speed+"km/h"
+
+    if(data.weather[0].main=="Clouds")
+    {
+        weatherIcon.src="clouds.png"
+    }else if(data.weather[0].main=="Clear")
+    {
+        weatherIcon.src="clear.png"
+    }else if(data.weather[0].main=="Rain")
+    {
+        weatherIcon.src="rain.png"
+    }else if(data.weather[0].main=="Mist")
+    {
+        weatherIcon.src="mist.png"
+    }else if(data.weather[0].main=="Drizzle")
+    {
+        weatherIcon.src="drizzle.png"
+    }
+    document.querySelector(".weather").style.display="block"
+    document.querySelector(".error").style.display="none"
+}
+}
+
+searchbtn.addEventListener("click",()=>{
+    checkWeather(searchbox.value);
+})
+
